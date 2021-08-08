d20f920e9a241b3cfff89c4c91f61a30878d6313
Jose Rincon <jrdydx@gmail.com>
2021-08-07 23:57:56 -0700

    Part One of Lab 1 is done

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,13 @@
+print("Lab 1 ")
+print("Created by Jose Rincon\n")
+
+print("Task 1: Installing Python and Libs - done")
+print("Task 2: Installing Pycharm - done")
+print("Task 3: Installing Git - done\n")
+
+print("Now Testing different escape codes")
+print("The First one is used for escaping a quotation")
+print("To escape a quotation, use the \\ back-slash before the quotation ---> \\\"")
+print("My name, \"Jose\",  in quotations would look like this in code:  \\\"Jose\\\"  \n")
+print("There's also an escape for new lines, just type \\n \nand a new line will be created")
+print("The thing about the \\n escape is that spaces matter. So don't put a space after \\n")
